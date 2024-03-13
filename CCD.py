4707e8087cd56dca67f308dfe7b370bc6f567186
wardcp11 <114361824+wardcp11@users.noreply.github.com>
2024-03-13 14:06:00 -0500

    Update CCD.py
    added things

--- CCD.py
+++ CCD.py
@@ -3,7 +3,7 @@
 #Readme First Time Setup:
 #   Need to "pip install thorcam" if not installed
 #   Run program once and take camID then insert below
-camID = ''
+camID = '12152'
 ##################################################################
 
 import numpy as np
@@ -13,8 +13,6 @@ from thorcam.camera import ThorCam
 #variable to give serial connection time to initialize
 sleepPeriod = 0.05
 
-
-
 class MyThorCam(ThorCam):
     def received_camera_response(self, msg, value):
         super(MyThorCam, self).received_camera_response(msg, value)
@@ -23,14 +21,16 @@ class MyThorCam(ThorCam):
         print('Received "{}" with value "{}"'.format(msg, value))
     def got_image(self, image, count, queued_count, t):
         print('Received image "{}" with time "{}" and counts "{}", "{}"'
-              .format(image, t, count, queued_count))
+        .format(image, t, count, queued_count))
 
 
-def camInit():
+def camInit(cam):
     try:
-        cam = MyThorCam()
+        time.sleep(sleepPeriod)
         cam.start_cam_process()
+        time.sleep(sleepPeriod)
         cam.refresh_cameras()
+        time.sleep(sleepPeriod)
         if camID == '':
             print("Camera ID not setup")
             print(cam)
@@ -43,13 +43,24 @@ def camInit():
 
             #Add camera set setting to change values
             #cam.set_setting(field, value)
+            cam.set_setting('exposure_ms', 150)
             return True
     except:
         print("Failed Initialization")
         return False
 
+def takePicture(cam):
+    cam.play_camera()
+    time.sleep(3)
+    cam.stop_playing_camera()
+    cam.close_camera()
+    cam.stop_cam_process(join=True)
+
 
 if __name__ == '__main__':
-    if camInit() == False:
+    cam = MyThorCam()
+    if camInit(cam) == False:
         print("Exiting")
     else:
+        print("Success")
+        takePicture(cam)
