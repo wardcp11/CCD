11486cd9144198d21b1a9846d54eff678cde7df5
Chase W <wardcp11@gmail.com>
2024-03-30 10:45:26 -0500

    test image1 test image2 test image3 test image4 ? huge changes

--- imgprocess.py
+++ imgprocess.py
@@ -1,14 +1,11 @@
 from PIL import Image
+from PIL import ImageFilter
+from PIL import ImageColor
 import numpy as np
-import multiprocessing as mp
 import time
+import colorsys
 
-def imgProcess(imgFile1,imgFile2,imgFile3,imgFile4, width, height, shared_array, lambdaC):
-
-    im1 = Image.open(imgFile1)
-    im2 = Image.open(imgFile2)
-    im3 = Image.open(imgFile3)
-    im4 = Image.open(imgFile4)
+def imgProcess():
 
     for y in range(height):
         for x in range(width):
@@ -21,25 +18,16 @@ def imgProcess(imgFile1,imgFile2,imgFile3,imgFile4, width, height, shared_array,
                     theta = np.pi / 2
                 else:
                     theta = -1 * (np.pi / 2)
+            phaseVals[y,x] = theta
             OH = (lambdaC * theta) / (4 * np.pi)
-            RH = OH / (1.3 - 1)
-            shared_array[y * width + x] = RH
-
-def arrayConversion(inArry, width, height):
-    arry = [[0 for i in range(width)] for j in range(height)]
-
-    for current in range(len(inArry)):
-        x = current % width
-        y = current // width
-        arry[y][x] = inArry[x+y]
-
-    return arry
+            RH = OH
+            arrayVals[y,x] = RH
     
 
 if __name__ == '__main__':
     startTime = time.time()
 
-    lambdaC = 500 #central wavelength
+    lambdaC = 600 #central wavelength
     imgFile1 = '1.tiff' #I(0)
     imgFile2 = '2.tiff' #I(pi/2)
     imgFile3 = '3.tiff' #I(3pi/2)
@@ -49,21 +37,32 @@ if __name__ == '__main__':
     im3 = Image.open(imgFile3)
     im4 = Image.open(imgFile4)
 
-    shared_array = mp.Array('f', im1.size[0] * im1.size[1])
     width = im1.size[0]
     height = im1.size[1]
+    arrayVals = np.empty((height, width))
+    phaseVals = np.empty((height, width))
 
-    arrProcess = mp.Process(target=imgProcess, args=(imgFile1,imgFile2,imgFile4,imgFile4, width, height, shared_array, lambdaC))
-    arrProcess.start()
-    arrProcess.join()
-
-    #print(list(shared_array))
+    imgProcess()
 
     im1.close()
     im2.close()
     im3.close()
     im4.close()
-    #print(list(shared_array))
-    #print(arrayConversion(list(shared_array), width, height))
+
+    phaseVals = phaseVals + (np.pi/2)
+    phaseVals = phaseVals * (255 / np.pi)
+    for y in range(height):
+        for x in range(width):
+            phaseVals[y,x] = int(round(phaseVals[y,x]))
+
+    
+    print(np.max(phaseVals))
+    im = Image.fromarray(phaseVals)
+    im = im.convert("RGB")
+
+    #im = im.filter(ImageFilter.GaussianBlur(2)) #image low pass filter with radius
+
+    im.save("filtered.png")
+    im.close()
 
     print("Done in:", time.time() - startTime, "Seconds")
